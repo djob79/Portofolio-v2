19e092206e9440c931c493745171bc3baa6731bf
djob79 <149446661+djob79@users.noreply.github.com>
2025-01-23 08:12:41 -0500

    Update ArticleView.vue

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -38,7 +38,7 @@ export default {
     methods: {
         async getDetails() {
             const id = this.route.params.id;
-            axios.get('https://64a38c9cc3b509573b564183.mockapi.io/api/blog/all/' + id)
+            axios.get('' + id)
                 .then(response => {
                     this.title = response.data.title;
                     this.image = response.data.image;
