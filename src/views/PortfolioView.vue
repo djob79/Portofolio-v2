dc092e108c8e3ba6f9015644fe77407d74a33d80
djob79 <149446661+djob79@users.noreply.github.com>
2025-01-30 17:52:54 -0500

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -6,48 +6,12 @@ export default {
       items: [
         {
           id: 1,
-          name: 'McDonalds Hangout v2',
+          name: 'WhosFrench Millitary / WhosFrench British Army',
           imageUrl: 'noFilte.webp',
-          status: 'A game I tried making by challenging myself using free models ',
-          tech: 'Luau, ',
+          status: 'Still up - Sandhurt copy/Made. I tried making it look as realistic and look like sandhurst british army.',
+          tech: 'Luau',
           github: '',
-          demo: 'https://www.roblox.com/games/18555001230/McDonalds-Hangout-Voice-Chat-New-UPDATE/'
-        },
-        {
-          id: 2,
-          name: 'Sahabat Muslim',
-          imageUrl: 'sahabatmuslim',
-          status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
-          tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/muslim-companion',
-          demo: 'https://sahabat-muslim.netlify.app/'
-        },
-        {
-          id: 3,
-          name: 'SISTEKDES',
-          imageUrl: 'sistekdes',
-          status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
-          tech: 'Laravel 9, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
-          id: 4,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
-          status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
-          tech: 'Laravel 8, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
-          id: 5,
-          name: 'SP2SE',
-          imageUrl: 'sp2se',
-          status: 'Sistem Percepatan Pelayanan Surat Elektronik, A web application to manage letters in the village.',
-          tech: 'Codeigniter 3, Bootstrap',
-          github: 'null',
-          demo: 'null'
+          demo: 'https://www.roblox.com/games/130288944902919/WhosFrench-British-Army'
         }
       ]
     };
